23e7fd6655bfae9fb9579aeb8227fd4f1fe80722
gifaalgifari <95413846+gifaalgifari@users.noreply.github.com>
2022-06-19 14:02:35 +0700

    Add files via upload

---
+++ pertemuan 3/struct.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+ 
+struct mahasiswa{
+    string nama;
+    string nim;
+    string jurusan;
+    string ipk;
+
+};
+ 
+int main(){
+ 
+    mahasiswa ms;
+    
+    ms.nama="muhammad mustopa albagdadi";
+    ms.nim="170699746";
+    ms.jurusan="alquran dan tafsir";
+    ms.ipk="3,7";
+    
+    cout<<"              DATA MAHASISWA       "<<endl;
+    cout<<"======================================"<<endl;
+    cout<<"Nama     : "<<ms.nama<<endl;
+    cout<<"NIM      : "<<ms.nim<<endl;
+    cout<<"Jurusan  : "<<ms.jurusan<<endl;
+    cout<<"IPK      : "<<ms.ipk<<endl;
+      
+    return 0;
+}
